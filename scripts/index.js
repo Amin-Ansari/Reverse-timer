0687d2adc0faa911c6d542bc3d343e69a31c8b94
Amin Ansari <devansari@yahoo.com>
2022-05-04 15:30:50 +0430

    The initial files and codes added

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+let buttoms = document.querySelectorAll("button");
+let second = document.querySelector(".second");
+let minute = document.querySelector(".minute");
+let hour = document.querySelector(".hour");
+let minuteInpute = document.querySelector("input");
